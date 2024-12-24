d50cbc3ce8483a25e47b9157cbe511aea55fe976
Duc Nguyen <20131039@student.hust.edu.vn>
2024-12-24 21:19:38 +0700

    home

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:money_flow/theme/custom_colors_theme.dart';
 
 final counterProvider = StateProvider((ref) => 0);
 
@@ -9,6 +10,36 @@ class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var widthS = MediaQuery.of(context).size.width;
+    final colors = Theme.of(context).extension<CustomColorsTheme>()!;
+    Widget item(String title, {isLine = true}) {
+      return Container(
+        width: widthS / 3 - 15,
+        height: 50,
+        decoration: isLine
+            ? BoxDecoration(
+                border: Border(
+                  right: BorderSide(
+                      color: colors.characterColor,
+                      width: 0.5), // Màu cho cạnh trái
+                ),
+              )
+            : null,
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: TextStyle(color: colors.characterColor),
+            ),
+            Text(
+              '200',
+              style: TextStyle(
+                  color: colors.characterColor, fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+      );
+    }
+
     return Center(
       // Consumer is a builder widget that allows you to read providers.
       child: Consumer(
@@ -23,18 +54,9 @@ class Home extends ConsumerWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(
-                  width: widthS / 3 - 15,
-                  child: Text('VNINDEX'),
-                ),
-                SizedBox(
-                  width: widthS / 3 - 15,
-                  child: Text('HNXINDEX'),
-                ),
-                SizedBox(
-                  width: widthS / 3 - 15,
-                  child: Text('UPINDEX'),
-                ),
+                item('VNINDEX'),
+                item('HNXINDEX'),
+                item('UPINDEX', isLine: false),
               ],
             ),
           );
